d91ffcbbb2b0afde302be03155cac178bce3e66c
Arkadipta Kundu <arkadiptak49@gmail.com>
2023-06-11 19:21:51 +0530

    dress 1 dlt

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
                     </div>  
                     <div class="col-md-4"> 
                     <div class="card">
-  <img src="./images/dress.jpg" class="card-img-top" alt="...">
+  <img src="imgs/dress.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
