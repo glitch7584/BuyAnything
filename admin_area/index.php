64508a0e5a87abf20c9e63dc761c6bbea70b6325
Arkadipta Kundu <arkadiptak49@gmail.com>
2023-06-13 10:11:29 +0530

    admin pannel design compleate with footer

--- admin_area/index.php
+++ admin_area/index.php
@@ -12,12 +12,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
         .admin_image {
-            width: 5%;
+            width: 100px;
             object-fit: contain;
         }
 
         .bg-buttoncol {
             background-color: brown;
+        }
+        .footer{
+            position: absolute;
+            width: 100%;
+            bottom: 0;
         }
     </style>
 </head>
@@ -46,28 +51,31 @@
         </div>
         <!-- third child -->
         <div class="row">
-            <div class="col-md-12 bg-secendary p-1">
-                <div>
+            <div class="col-md-12 bg-secendary p-1 d-flex align-items-center">
+                <div class="p-5">
                     <a href="#"><img src="../imgs/apple.jpg" class="admin_image">
-                        <p class="text-light text-center"> Admin name</p>
+                        <p class="text-light text-center p-0"> Admin name</p>
                     </a>
                 </div>
-                <div class="button text-center">
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">Insert Products</a>
-                    </button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">View Products</a>
-                    </button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">Insert Catagoreies</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">View Catagories</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">Insert Brands</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">View Brands</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">All Orders</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">All Payments</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">list users</a></button>
-                    <button><a href="" class="nav-link text-light bg-buttoncol my-1">Logout</a></button>
+                <div class="">
+                    <div class="button text-center m-1">
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">view Products</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Catagories</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Catagories</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="footer p-3 text-center footer">
+        <p>footer</p>
     </div>
     <!-- js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
